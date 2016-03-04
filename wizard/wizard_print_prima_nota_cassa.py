fd70b0641fe7bf11a9a3092d9b73d82109b84bcf
livglm <maxmauro@solarisnet.it>
2016-03-04 17:42:35 +0100

    menu

--- wizard/wizard_print_prima_nota_cassa.py
+++ wizard/wizard_print_prima_nota_cassa.py
@@ -64,7 +64,7 @@ class account_report_prima_nota(models.TransientModel):
 
     def lines(self, main_account):
         """ Return all the account_move_line of account with their account code counterparts """
-        account_ids = self._get_children_accounts(main_account)
+        account_ids = self._get_children_accounts(1)
 
         move_state = ['draft','posted']
         if self.target_move == 'posted':
