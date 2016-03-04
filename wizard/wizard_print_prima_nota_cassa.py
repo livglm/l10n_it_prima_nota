a620606de8d22ca9b1d750df824789929189c16c
livglm <maxmauro@solarisnet.it>
2016-03-04 18:03:05 +0100

    menu

--- wizard/wizard_print_prima_nota_cassa.py
+++ wizard/wizard_print_prima_nota_cassa.py
@@ -128,6 +128,7 @@ class account_report_prima_nota(models.TransientModel):
         self.sortby = 'sort_date'
         #
         id = self.lines
+        print id
          #data = self.pre_print_report()
 
         #data['form'].update(self.read(['landscape',  'initial_balance', 'amount_currency', 'sortby'])[0])
