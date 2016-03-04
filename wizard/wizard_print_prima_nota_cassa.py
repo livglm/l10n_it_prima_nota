ae2d1b6eedb9f44173db06432f28846fa351b478
livglm <maxmauro@solarisnet.it>
2016-03-04 18:08:11 +0100

    menu

--- wizard/wizard_print_prima_nota_cassa.py
+++ wizard/wizard_print_prima_nota_cassa.py
@@ -146,7 +146,7 @@ class account_report_prima_nota(models.TransientModel):
         }
 
         #return { 'type': 'ir.actions.report.xml', 'report_name': 'account.print.prima_nota_cassa', 'datas': data}
-        return { 'type': 'ir.actions.report.xml','report_name': 'ln10_it_prima_nota.prima_nota', 'datas': datas}
+        #return { 'type': 'ir.actions.report.xml','report_name': 'ln10_it_prima_nota.prima_nota', 'datas': datas}
 
         #return self.pool.get('account_report_prima_nota').get_action(cr, uid, [], 'ln10_it_prima_nota.prima_nota', data=data)
 
